ff1fddd27a1a735c893c95bb0c5a0213583ff1ef
Haroen Viaene <hello@haroen.me>
2018-01-03 18:19:09 +0100

    enable cors

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const {send} = require('micro');
+const { send } = require('micro');
+const cors = require('micro-cors')();
 
 let match = require('fs-router')(__dirname + '/routes');
 
-module.exports = async function(req, res) {
+module.exports = cors(async function(req, res) {
   let matched = match(req);
   if (matched) return await matched(req, res);
-  send(res, 404, {error: 'Not found'});
-};
+  send(res, 404, { error: 'Not found' });
+});
