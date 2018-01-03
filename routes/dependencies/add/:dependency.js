e3fe9f40449e9d83d003236833754b686db21c68
Haroen Viaene <hello@haroen.me>
2018-01-03 17:56:53 +0100

    feat: endpoint for adding and removing deps

---
+++ routes/dependencies/add/:dependency.js
@@ -0,0 +1,15 @@
+const { send } = require('micro');
+const hasYarn = require('has-yarn');
+const execa = require('execa');
+
+module.exports = async ({ params: { dependency }, on }, res) => {
+  try {
+    const command = hasYarn() ? 'yarn' : 'npm';
+    const { stdout, kill } = execa(command, ['add', dependency]);
+    stdout.pipe(process.stdout);
+    send(res, 200, stdout);
+    on('close', kill);
+  } catch (err) {
+    send(res, 404, err);
+  }
+};
