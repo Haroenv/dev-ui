bdb1ca0ee87e5869122ff491412ba23d017a97c7
Lucas Azzola <derflatulator@gmail.com>
2018-01-04 21:45:16 +1100

    feat: add ui setup

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const { send } = require('micro');
 const cors = require('micro-cors')();
 
+let static = require('./src/serve')('./dist');
 let match = require('fs-router')(__dirname + '/routes');
 
 module.exports = cors(async function(req, res) {
   let matched = match(req);
   if (matched) return await matched(req, res);
-  send(res, 404, { error: 'Not found' });
+  return await static(req, res);
 });
---
+++ src/serve.js
@@ -0,0 +1,32 @@
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
+const { send } = require('micro');
+const mime = require('mime');
+
+// https://github.com/zeit/serve/issues/267
+
+module.exports = (prefix = './') => async (req, res) => {
+  const parseUrl = url.parse(req.url);
+  let file = `${prefix}${parseUrl.pathname}`;
+
+  fs.exists(file, exist => {
+    if (!exist) {
+      send(res, 404);
+      return;
+    }
+
+    if (fs.statSync(file).isDirectory()) {
+      file += '/index.html';
+    }
+
+    fs.readFile(file, (err, data) => {
+      if (err) {
+        send(res, 500);
+      } else {
+        res.setHeader('Content-type', mime.getType(file));
+        send(res, 200, data);
+      }
+    });
+  });
+};
