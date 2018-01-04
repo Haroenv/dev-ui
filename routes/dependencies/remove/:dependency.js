4102514f7b8eed104b8d8fca1332d2843ff2a523
Lucas Azzola <derflatulator@gmail.com>
2018-01-05 03:24:56 +1100

    fix: fix event listeners in dependencies add/remove

--- routes/dependencies/remove/:dependency.js
+++ routes/dependencies/remove/:dependency.js
@@ -2,13 +2,14 @@ const { send } = require('micro');
 const hasYarn = require('has-yarn');
 const execa = require('execa');
 
-module.exports = async ({ params: { dependency }, on }, res) => {
+module.exports = async (req, res) => {
+  const { params: { dependency } } = req;
   try {
     const command = hasYarn() ? 'yarn' : 'npm';
     const { stdout, kill } = execa(command, ['remove', dependency]);
     stdout.pipe(process.stdout);
     send(res, 200, stdout);
-    on('close', kill);
+    req.on('close', kill);
   } catch (err) {
     send(res, 404, err);
   }
