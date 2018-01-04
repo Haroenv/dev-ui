31c523a547b7760ff95ac7b5296ab64de92c2d48
ranfdev <ranfdev@gmail.com>
2018-01-04 16:21:15 +0100

    more compact list

--- ui/components/DependenciesList.jsx
+++ ui/components/DependenciesList.jsx
@@ -3,7 +3,12 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 const Section = styled.section`
-  padding: 1em;
+  padding: 5px;
+  & h2,
+  & code {
+    margin: 0;
+    margin-bottom: 10px;
+  }
 `;
 
 const DependenciesList = ({ dependencies }) =>
