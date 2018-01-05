13a2a552b03d0823587c7c98734d8145f75bcb7a
ranfdev <ranfdev@gmail.com>
2018-01-05 17:33:28 +0100

    removed unnecessary console log

--- bin/dev-ui.js
+++ bin/dev-ui.js
@@ -6,6 +6,5 @@ const opener = require('opener');
 const app = require('..');
 getPort().then(port => {
   micro(app).listen(port);
-  console.log(`opened at http://127.0.0.1:${port}`);
   opener(`http://127.0.0.1:${port}`);
 });
