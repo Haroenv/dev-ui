70002cb4254da0848cd5f8c5303b9e4b373af531
ranfdev <ranfdev@gmail.com>
2018-01-05 12:49:12 +0100

    New component: list item. Edited lists to use it. Added "remove" button to dependencies

--- ui/components/DependenciesList.jsx
+++ ui/components/DependenciesList.jsx
@@ -1,22 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
 
-const Section = styled.section`
-  padding: 5px;
-  & h2,
-  & code {
-    margin: 0;
-    margin-bottom: 10px;
-  }
-`;
+import ListItem from './ListItem';
 
 const DependenciesList = ({ dependencies }) =>
   dependencies.map(({ name, version }) => (
-    <Section key={name}>
-      <h2>{name}</h2>
-      <code>{version}</code>
-    </Section>
+    <ListItem
+      title={name}
+      subtitle={version}
+      buttons={<button>remove</button>}
+      key={name}
+    />
   ));
 
 DependenciesList.propTypes = {
--- ui/components/ScriptList.jsx
+++ ui/components/ScriptList.jsx
@@ -2,26 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-import Section from './Section';
-
+import ListItem from './ListItem';
 const Container = styled.div``;
-
-const Heading = styled.h2`
-  font-size: 1.5em;
-`;
-
-const Command = styled.div`
-  padding-left: 1em;
-  font-family: monospace;
-`;
 
 const ScriptList = ({ scripts = [], onScriptClick = () => {} }) => (
   <Container>
     {scripts.map(({ name, command }) => (
-      <Section key={name} onClick={() => onScriptClick(name)}>
-        <Heading>{name}</Heading>
-        <Command>{command}</Command>
-      </Section>
+      <ListItem
+        title={name}
+        subtitle={command}
+        key={name}
+        buttons={<button onClick={() => onScriptClick(name)}>run</button>}
+      />
     ))}
   </Container>
 );
