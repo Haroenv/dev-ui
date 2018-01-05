bc6eecbda1e198ce65fdd511e27cc355c7d647de
Raathigeshan Kugarajan <raathigesh92@gmail.com>
2018-01-05 20:38:27 +1100

    Use custom paths for routes instead of file name based convention #17

--- routes/dependencies/remove/dependency.js
+++ routes/dependencies/remove/dependency.js
@@ -4,10 +4,12 @@ const ansiCommandStream = require('../../../src/ansiCommandStream');
 module.exports = async (req, res) => {
   const { params: { dependency } } = req;
   try {
-    const { stream, kill } = ansiCommandStream({ args: ['add', dependency] });
+    const { stream, kill } = ansiCommandStream({ args: ['rm', dependency] });
     send(res, 200, stream);
     req.on('close', kill);
   } catch (err) {
     send(res, 404, err);
   }
 };
+
+module.exports.path = '/dependencies/remove/:dependency';
--- routes/scripts/run/script.js
+++ routes/scripts/run/script.js
@@ -2,12 +2,16 @@ const { send } = require('micro');
 const ansiCommandStream = require('../../../src/ansiCommandStream');
 
 module.exports = async (req, res) => {
-  const { params: { dependency } } = req;
+  const { params: { script } } = req;
   try {
-    const { stream, kill } = ansiCommandStream({ args: ['rm', dependency] });
+    const { stream, kill } = ansiCommandStream({ args: ['run', script] });
+    // stream.pipe(process.stdout);
     send(res, 200, stream);
     req.on('close', kill);
   } catch (err) {
+    process.stderr.write(err.toString());
     send(res, 404, err);
   }
 };
+
+module.exports.path = '/scripts/run/:script';
