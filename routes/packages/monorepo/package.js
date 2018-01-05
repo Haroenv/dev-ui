8d91fe81dcc691020177be9252074bea4bd73edc
Lucas Azzola <derflatulator@gmail.com>
2018-01-05 22:54:19 +1100

    fix: fix path field

--- routes/packages/monorepo/package.js
+++ routes/packages/monorepo/package.js
@@ -1,9 +1,11 @@
+const { send } = require('micro');
 const { getMonoRepoPackages } = require('../../../src/packageUtils.js');
 
-module.exports = async req => {
+module.exports = async (req, res) => {
   const { params: { package: packageName } } = req;
   const all = await getMonoRepoPackages();
-  return all.find(name => name === packageName);
+  const pkg = all.find(({ name }) => name === packageName);
+  send(res, 200, pkg);
 };
 
-module.exports.path = '/packages/monrepo/:package';
+module.exports.path = '/packages/monorepo/:package';
