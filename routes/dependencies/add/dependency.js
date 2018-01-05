78da7c033fd4801aa25976ec21559f4c42d01146
Raathigeshan Kugarajan <raathigesh92@gmail.com>
2018-01-05 01:40:47 -0800

    chore: Use custom paths for routes instead of file name based convention (#20)
    fixes #17

--- routes/dependencies/add/dependency.js
+++ routes/dependencies/add/dependency.js
@@ -2,14 +2,14 @@ const { send } = require('micro');
 const ansiCommandStream = require('../../../src/ansiCommandStream');
 
 module.exports = async (req, res) => {
-  const { params: { script } } = req;
+  const { params: { dependency } } = req;
   try {
-    const { stream, kill } = ansiCommandStream({ args: ['run', script] });
-    // stream.pipe(process.stdout);
+    const { stream, kill } = ansiCommandStream({ args: ['add', dependency] });
     send(res, 200, stream);
     req.on('close', kill);
   } catch (err) {
-    process.stderr.write(err.toString());
     send(res, 404, err);
   }
 };
+
+module.exports.path = '/dependencies/add/:dependency';
