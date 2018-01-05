d7eb6140d8bb08f014ae088a4d584558cc07bbeb
ranfdev <ranfdev@gmail.com>
2018-01-05 17:12:04 +0100

    takeLatest REMOVE_DEP

--- ui/sagas/index.js
+++ ui/sagas/index.js
@@ -2,6 +2,8 @@ import { takeLatest, fork } from 'redux-saga/effects';
 
 import { RUN_SCRIPT } from '../ducks/scripts';
 import { SET_SELECTED_PACKAGE } from '../ducks/packages';
+import { REMOVE_DEP } from '../ducks/dependencies';
+import { removeDependency } from './dependencies';
 import { runScript } from './scripts';
 import { fetchRootPackage, fetchMonoRepoPackage } from './packages';
 
@@ -9,4 +11,5 @@ export default function* index() {
   yield fork(fetchRootPackage);
   yield takeLatest(RUN_SCRIPT, runScript);
   yield takeLatest(SET_SELECTED_PACKAGE, fetchMonoRepoPackage);
+  yield takeLatest(REMOVE_DEP, removeDependency);
 }
