05ea0a5cb05162bdd0211bce91e046bafb197784
ranfdev <ranfdev@gmail.com>
2018-01-06 14:45:33 +0100

    Add dependency reducer, action and saga

--- ui/sagas/index.js
+++ ui/sagas/index.js
@@ -2,7 +2,7 @@ import { takeLatest, fork } from 'redux-saga/effects';
 
 import { RUN_SCRIPT } from '../ducks/scripts';
 import { SET_SELECTED_PACKAGE } from '../ducks/packages';
-import { REMOVE_DEPENDENCY } from '../ducks/dependencies';
+import { REMOVE_DEPENDENCY, ADD_DEPENDENCY } from '../ducks/dependencies';
 import { removeDependency } from './dependencies';
 import { runScript } from './scripts';
 import { fetchRootPackage, fetchMonoRepoPackage } from './packages';
@@ -12,4 +12,5 @@ export default function* index() {
   yield takeLatest(RUN_SCRIPT, runScript);
   yield takeLatest(SET_SELECTED_PACKAGE, fetchMonoRepoPackage);
   yield takeLatest(REMOVE_DEPENDENCY, removeDependency);
+  yield takeLatest(ADD_DEPENDENCY, removeDependency);
 }
